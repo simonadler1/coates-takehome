0bd70935018497dbc8f4f7772cc55fd37e4db2e0
simon adler <adler.shimon@gmail.com>
2024-06-29 23:12:12 -0500

    changes

--- coates-weather-frontend/api/weatherService.ts
+++ coates-weather-frontend/api/weatherService.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/weather";
+const API_ROOT = process.env.BACKEND_API_ROOT || "http://localhost:5000";
+
+const API_URL = `${API_ROOT}/api/weather`;
 
 export const fetchWeather = async (city: string) => {
   const response = await axios.get(`${API_URL}/${city}`);
