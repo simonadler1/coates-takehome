a42ac56a6e4b4b4888ffa7733c9bb1ce4847b373
simon adler <adler.shimon@gmail.com>
2024-06-29 23:53:23 -0500

    changes

--- coates-weather-frontend/app/api/weatherService.ts
+++ coates-weather-frontend/app/api/weatherService.ts
@@ -1,19 +1,14 @@
-const API_ROOT = process.env.BACKEND_API_ROOT || "http://localhost:5000";
+const API_ROOT = process.env.NEXT_PUBLIC_BACKEND_API_ROOT || "http://localhost:5000";
 
 const API_URL = `${API_ROOT}/api/weather`;
 
 export const fetchWeather = async (city: string) => {
   let response: any;
-  fetch(`${API_URL}/${city}`).then((res) => {
-    response = res.json();
-  });
-  if (!response) {
+  response = await fetch(`${API_URL}/${city}`);
+  if (!response.ok) {
     throw new Error("Failed to fetch weather data");
   }
-  if (!response.data) {
-    throw new Error("Failed to fetch weather data");
-  }
-
-  return response.data;
+  const data = await response.json();
+  return data;
 };
 export const runtime = "edge";
