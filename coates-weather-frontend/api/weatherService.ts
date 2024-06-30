07d300a4ccdce2fd0b198c98dc602278b3a1dabc
simon adler <adler.shimon@gmail.com>
2024-06-29 23:32:37 -0500

    dont use axios

--- coates-weather-frontend/api/weatherService.ts
+++ coates-weather-frontend/api/weatherService.ts
@@ -1,11 +1,18 @@
-import axios from "axios";
-
 const API_ROOT = process.env.BACKEND_API_ROOT || "http://localhost:5000";
 
 const API_URL = `${API_ROOT}/api/weather`;
 
 export const fetchWeather = async (city: string) => {
-  const response = await axios.get(`${API_URL}/${city}`);
+  let response: any;
+  fetch(`${API_URL}/${city}`).then((res) => {
+    response = res.json();
+  });
+  if (!response) {
+    throw new Error("Failed to fetch weather data");
+  }
+  if (!response.data) {
+    throw new Error("Failed to fetch weather data");
+  }
 
   return response.data;
 };
